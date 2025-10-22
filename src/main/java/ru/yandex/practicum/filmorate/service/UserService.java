3d40e0b8e1c871ec6cc716ed1cc6ec01f5fce324
Marina Shtrassheim <mf.shtrasgeym@mail.ru>
2025-10-22 11:57:18 +0500

    add-database v1 fix api6

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.CanNotBeAddedAsFriendException;
-import ru.yandex.practicum.filmorate.exception.CanNotBeRemovedFromFriendException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
